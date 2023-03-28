f8ac60aec0318232f11ec3fd464bd1083328322f
NUCCASJNR <adegbitealareef28@yahoo.com>
2023-03-28 10:53:33 +0100

    update

--- Binary_trees/insert_tree.py
+++ Binary_trees/insert_tree.py
@@ -1,12 +1,18 @@
 #!/use/bin/python3
+"""This module explains the insertion operation in a binary tree
+"""
 
 class Bst:
+    """The Bst class"""
+
     def __init__(self, key):
         self.key = key
         self.left = None
         self.right = None
 
     def insert(self, data):
+        """Inserts the node"""
+
         if self.key is None:
             self.key = data
         if self.key == data:
@@ -24,6 +30,7 @@ class Bst:
     def print_tree(self):
         if self.left:
             self.left.print_tree()
-        print(self.key)
+        print(self.key, end=" ")
+        print()
         if self.right:
             self.right.print_tree()
